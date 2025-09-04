f03b271b8383bc9b28deadcbc410ca74b7c1022b
ZacJ74 <zacharyjtx@gmail.com>
2025-09-04 13:37:13 -0500

    fixed issues with the community page

--- controllers/users.js
+++ controllers/users.js
@@ -2,8 +2,11 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user.js');
+const Recipe = require('../models/recipe.js'); 
 
-// GET /users - Community Index Page
+
+
+// Community Index Page (GET)
 router.get('/', async (req, res) => {
   try {
     const allUsers = await User.find({});
@@ -15,5 +18,25 @@ router.get('/', async (req, res) => {
     res.redirect('/');
   }
 });
+
+
+
+// Show Route
+router.get('/:userId', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    const recipes = await Recipe.find({ owner: req.params.userId });
+    res.render('users/show.ejs', {
+      user: user,
+      recipes: recipes,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/users');
+  }
+}
+);
+
+
 
 module.exports = router;
